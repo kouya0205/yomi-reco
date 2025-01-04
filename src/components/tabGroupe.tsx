745655cf4a0a93427a29b7e825426fddc90e9fe1
DahuiSheng <sheng.131216@gmail.com>
2025-01-04 17:45:14 +0900

    アイコンを追加

--- src/components/tabGroupe.tsx
+++ src/components/tabGroupe.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import { Bookmark, BookmarkCheck, BookOpen } from 'lucide-react';
 
 type Props = {
   currentTab: string;
@@ -12,13 +13,22 @@ export default function TabGroup({ currentTab, onTabChange }: Props) {
     <Tabs value={currentTab} onValueChange={onTabChange} className="w-full">
       <TabsList className="flex w-full rounded-full">
         <TabsTrigger value="want" className="flex-1 text-center rounded-full">
-          読みたい
+          <div className="flex gap-2 items-center">
+            <div>読みたい</div>
+            <Bookmark />
+          </div>
         </TabsTrigger>
         <TabsTrigger value="reading" className="flex-1 text-center rounded-full">
-          読んでる
+          <div className="flex gap-2 items-center">
+            <div>読んでる</div>
+            <BookOpen />
+          </div>
         </TabsTrigger>
         <TabsTrigger value="done" className="flex-1 text-center rounded-full">
-          読んだ
+          <div className="flex gap-2 items-center">
+            <div>読んだ</div>
+            <BookmarkCheck />
+          </div>
         </TabsTrigger>
       </TabsList>
       <TabsContent value="want" />
