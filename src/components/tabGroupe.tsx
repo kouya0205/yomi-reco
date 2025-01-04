94cb183e0d2f0cdeea4d44cff72aeb0ede055b64
DahuiSheng <sheng.131216@gmail.com>
2025-01-04 17:39:14 +0900

    タブのサイズ変更

--- src/components/tabGroupe.tsx
+++ src/components/tabGroupe.tsx
@@ -9,11 +9,17 @@ type Props = {
 
 export default function TabGroup({ currentTab, onTabChange }: Props) {
   return (
-    <Tabs value={currentTab} onValueChange={onTabChange}>
-      <TabsList>
-        <TabsTrigger value="want">読みたい</TabsTrigger>
-        <TabsTrigger value="reading">読んでる</TabsTrigger>
-        <TabsTrigger value="done">読んだ</TabsTrigger>
+    <Tabs value={currentTab} onValueChange={onTabChange} className="w-full">
+      <TabsList className="flex w-full rounded-full">
+        <TabsTrigger value="want" className="flex-1 text-center rounded-full">
+          読みたい
+        </TabsTrigger>
+        <TabsTrigger value="reading" className="flex-1 text-center rounded-full">
+          読んでる
+        </TabsTrigger>
+        <TabsTrigger value="done" className="flex-1 text-center rounded-full">
+          読んだ
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="want" />
       <TabsContent value="reading" />
