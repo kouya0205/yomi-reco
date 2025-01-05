f19cf7d73b2b7f6a35c7af23fbd7958bbb605a53
DahuiSheng <sheng.131216@gmail.com>
2025-01-05 19:48:23 +0900

    スタイルを整える

--- src/components/avatarList.tsx
+++ src/components/avatarList.tsx
@@ -15,12 +15,11 @@ export const AvatarList: React.FC<AvatarListProps> = ({ users }: AvatarListProps
       <ScrollArea className="w-full rounded-md overflow-x-auto">
         <div className="flex gap-4 p-4">
           {users.map((user, index) => (
-            <div key={index}>
+            <div key={index} className="gap-4 flex flex-col justify-center">
               <Link href={`/bookshelf/${user.user_id}`}>
                 <div className="flex flex-col items-center space-y-2 cursor-pointer">
-                  <Avatar className="w-20 h-20 ring-2 ring-offset-2 ring-blue-500 hover:ring-offset-blue-300 transition-all">
+                  <Avatar className="w-20 h-20 ring-2 ring-offset-2 ring-blue-500 hover:ring-offset-blue-300 transition-all rounded-full">
                     <AvatarImage src={user.avatar_url || ''} />
-                    <AvatarFallback>{user.name?.charAt(0)}</AvatarFallback>
                   </Avatar>
                 </div>
               </Link>
